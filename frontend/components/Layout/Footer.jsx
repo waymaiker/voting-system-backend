f67eb2e6ebcb46f11d29eedeb06def3b2e76a0be
Bonkomprann <waymaiker@laposte.net>
2023-02-01 07:11:10 +0100

    Remove frontend

--- frontend/components/Layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Text } from '@chakra-ui/react'
-
-export default function Footer() {
-    return (
-      <Flex h="10vh" p="2rem" justifyContent="center">
-          <Text> © by Watson {new Date().getFullYear()} </Text>
-      </Flex>
-    )
-}
--- frontend/components/Proposals/ProposalSkeleton.jsx
+++
@@ -1,7 +0,0 @@
-import { Flex, Text } from '@chakra-ui/react'
-
-export const ProposalSkeleton = () =>
-  <Flex direction="column" alignItems="Center" p="5">
-    <Text p="5" fontSize="2xl" fontWeight="bold" color="black"> No registered voters yet</Text>
-  </Flex>
- 
